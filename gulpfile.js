44a3adb8290da89964fa17f9ff0051f8058b71f4
KurienPhilip <kurien.philip@burgessgroup.com>
2018-05-25 10:50:10 -0400

    Initial Setup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+//style paths
+var sassFiles = 'assets/styles/sass/**/*.scss',
+    cssDest = 'assets/styles/css/';
+
+gulp.task('styles', function(){
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest))
+        .pipe(browserSync.reload({
+          stream: true
+        }));
+});
