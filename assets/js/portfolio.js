bef362daaa026e72ee33b6aa631dada127475b1f
KurienPhilip <kurien.philip@burgessgroup.com>
2018-06-05 12:42:09 -0400

    Update to styles and angular

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,4 +1,31 @@
 angular.module('portfolioApp', ['ngRoute','ngMaterial', 'ngMessages'])
+// Define and set Burgess color pallete as the default theme
+.config(['$mdThemingProvider', function ($mdThemingProvider) {
+    $mdThemingProvider.definePalette('portfolioPalette', {
+        '50': 'ffffff', // background
+        '100': 'B3E5FC',
+        '200': '81D4FA',
+        '300': 'A5B3FF', // primary 2
+        '400': '29B6F6',
+        '500': '3F51B5', // primary 1
+        '600': 'e44c3d', // highlight
+        '700': '0288D1',
+        '800': 'eb7109', // primary 3
+        '900': '01579B',
+        'A100': 'ffffff', // accent 2 - menu background
+        'A200': '3F51B5', // accent 1 - menu  font color 
+        'A400': 'A5B3FF', // accent 3
+        'A700': 'A5B3FF', // accent 4 
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+        // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+            '200', '300', '400', 'A100'],
+        'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+    $mdThemingProvider.theme('default')
+        .primaryPalette('portfolioPalette')
+        .accentPalette('portfolioPalette')
+        .backgroundPalette('portfolioPalette')
   .controller('PortfolioController', function() {
     var vm = this;
     vm.todos = [
