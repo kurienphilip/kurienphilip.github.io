ca06eee632a1111642b3467c942564d3c01086b7
Kurien Philip <kurien.philip@burgessgroup.com>
2022-02-07 13:10:02 -0500

    Updated view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,6 +30,10 @@ $(document).ready(function() {
     }
 });
 
+$(document).scroll(function () {
+  var $nav = $(".navbar-fixed-top");
+  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
 
   $(".navigation a").on('click', function(event) {
     if (this.hash !== "") {
