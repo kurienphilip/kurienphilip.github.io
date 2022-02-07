fada05a66997ff4dc4dde6773b2810b30d616340
Kurien Philip <kurien.philip@burgessgroup.com>
2022-02-07 13:24:46 -0500

    Updated view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,6 +30,8 @@ $(document).ready(function() {
     }
 });
 
+animateValue("expLbl", 1, 14, 1000);
+
 $(document).scroll(function () {
   var $nav = $(".navbar-fixed-top");
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
@@ -49,3 +51,20 @@ $(document).scroll(function () {
     }
   });
 });
+
+
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current+"+";
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+};
